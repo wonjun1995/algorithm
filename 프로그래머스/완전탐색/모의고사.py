3b62aacf711dfa502cf7c2070da81df3339f94ef
wonjun1995 <wonjun9506@gmail.com>
2021-12-20 16:15:06 +0900

    주석 추가

--- 프로그래머스/완전탐색/모의고사.py
+++ 프로그래머스/완전탐색/모의고사.py
@@ -21,3 +21,8 @@ def solution(answer):
     
     return result
 
+
+# enumerate 함수
+# 리스트가 있는 경우 순서와 리스트의 값을 전달하는 함수
+# 순서가 있는 자료형(list,set,tuple,dictionary,string)을 입력받아 인덱스 값을 포함하는 enumerate 객체 리턴
+# 보동 enumerate 함수는 for문과 함께 자주 사용
