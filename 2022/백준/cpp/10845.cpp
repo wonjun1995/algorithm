7686b93f2658aa3783cfd2fe022662dfbc32c860
wonjun <wonjun@sysofting.co.kr>
2022-02-08 17:55:27 +0900

    0208_큐

---
+++ 2022/백준/cpp/10845.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    queue<int> q;
+    for(int i=0; i<n; i++){
+        string input;
+        cin >> input;
+
+        if(input == "push"){
+            int x;
+            cin >> x;
+            q.push(x);
+        }else if(input == "pop"){
+            if(q.empty()){
+                cout << -1 << '\n';
+            }else{
+                cout << q.front() <<'\n';
+                q.pop();
+            }
+        }else if(input == "size"){
+            cout << q.size() << '\n';
+            
+        }else if(input == "empty"){
+            if(q.empty()){
+                cout << 1 << '\n';
+            }else{
+                cout << 0 << '\n';
+            }
+        }else if(input == "front"){
+            if(q.empty()){
+                cout << -1 << '\n';
+            }else{
+                cout << q.front() << '\n';
+            }
+            
+        }else if(input == "back"){
+            if(q.empty()){
+                cout << -1 << '\n';
+            }else{
+                cout << q.back() << '\n';
+            }
+        }
+    }
+
+    return 0;
+}
