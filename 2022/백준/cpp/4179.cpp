fa8ef2b7079e9138ea8dc1c025ad039ffc2326c8
wonjun1995 <wonjun9506@gmail.com>
2022-02-17 21:16:17 +0900

    0217_BFS불!

--- 2022/백준/cpp/4179.cpp
+++ 2022/백준/cpp/4179.cpp
@@ -1,8 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int board[1002][1002];
-int vis[1002][1002];
+string board[1002];
+int f_dist[1002][1002];
+int j_dist[1002][1002];
 int dx[4]={1,0,-1,0};
 int dy[4]={0,1,0,-1};
 
@@ -18,7 +19,74 @@ int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
 
+    int R,C;
+    cin >> R >> C;
 
+    for(int i=0;i<R;i++){
+        cin >> board[i];
+    }
 
+    for(int i=0;i < R;i++){
+        fill(f_dist[i],f_dist[i]+C,-1);
+        fill(j_dist[i],j_dist[i]+C,-1);
+    }
+
+    queue<pair<int,int>> fq;
+    queue<pair<int,int>> jq;
+    
+    //불 지훈 불 지훈
+    for(int i=0;i<R;i++){
+        for(int j=0;j<C;j++){
+            if(board[i][j]=='F'){
+                fq.push({i,j});
+                f_dist[i][j] = 0;
+            }
+            if(board[i][j]=='J'){
+                jq.push({i,j});
+                j_dist[i][j]= 0;
+            }
+        }
+    }
+    //불 BFS
+    while(!fq.empty()){
+        pair<int,int> cur = fq.front();fq.pop();
+        for(int dir=0;dir < 4;dir++){
+            int nx=cur.first+dx[dir];
+            int ny=cur.second+dy[dir];
+            if(nx<0||nx>=R || ny<0 || ny>=C) continue;
+            if(f_dist[nx][ny] >= 0 || board[nx][ny]=='#') continue;
+            f_dist[nx][ny] = f_dist[cur.first][cur.second]+1;
+            fq.push({nx,ny});
+        }
+    }
+
+/*     for(int i=0;i<R;i++){
+        for(int j=0;j < C;j++){
+            cout << f_dist[i][j]<<' ';
+        }
+        cout << '\n';
+    } */
+
+    //지훈 BFS
+    //BFS하는데 시간에 대한 비교를 어떻게 할 것인가??
+    //j가 f보다 작으면 이동가능?
+    while(!jq.empty()){
+        pair<int,int> cur=jq.front();jq.pop();
+        for(int dir=0;dir<4;dir++){
+            int nx=cur.first+dx[dir];
+            int ny=cur.second+dy[dir];
+            if(nx<0||nx>=R || ny<0 || ny>=C) {
+                cout << j_dist[cur.first][cur.second]+1;
+                return 0;
+            }
+            if(j_dist[nx][ny] >= 0 || board[nx][ny]=='#') continue;
+            if(f_dist[nx][ny] != -1 && j_dist[cur.first][cur.second]+1 >= f_dist[nx][ny]) continue;
+            j_dist[nx][ny] = j_dist[cur.first][cur.second]+1;
+            jq.push({nx,ny});
+        }
+    }
+
+    //불이 도달하기 전에 미로를 탈출 할 수 없는경우 IMPOSSIBLE 출력
+    cout << "IMPOSSIBLE";
     return 0;
 }
