4ee8429eab8a7fdfbf1023e16646272b5bfc0e14
wonjun1995 <wonjun9506@gmail.com>
2022-02-23 10:08:36 +0900

    0223_스택 수열

--- 2022/백준/cpp/10773.cpp
+++ 2022/백준/cpp/10773.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 //제로
-//
+//잘못된 수를 부를 때마다 0을 외쳐서, 가장 최근에 재민이가 쓴 수를 지우게 시킨다.(pop)
+//이렇게 모든 수를 받아 적은 후 그 수의 합을 구하시오.
 int main(){
 
+    int k;
+    cin >> k;
+    stack<int> stack;
+    while(k--){
+        int n;
+        cin >> n;
+        if(n == 0){
+            stack.pop();
+        }else{
+            stack.push(n);
+        }
+    }
+    int answer=0;
+    while(!stack.empty()){
+        answer+= stack.top();
+        stack.pop();
+    }
+    cout << answer;
     return 0;
 }
---
+++ 2022/백준/cpp/1874.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//스택 수열
+//1부터 n까지의 수를 스택에 넣었다가 뽑아 늘어놓음으로써, 하나의 수열을 만들 수 있음.
+//스택에 push하는 순서는 반드시 오름차순을 지키도록 함.
+//push연산은 +로, pop 연산은 -로 표현하도록 한다. 불가능한 경우 NO를 출력한다.
+int main(){
+    int n;
+    stack<int> st;
+    cin >> n;
+    while(n--){
+        int num;
+        cin >> num;
+    }
+
+    return 0;
+}
+
+// ++++--++-++-----
