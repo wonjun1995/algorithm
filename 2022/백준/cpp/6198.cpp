130a311c8570d01e18b689442615ed7208d4eb15
wonjun <wonjun@sysofting.co.kr>
2022-02-24 15:19:33 +0900

    0224_옥상정원꾸미기

--- 2022/백준/cpp/6198.cpp
+++ 2022/백준/cpp/6198.cpp
@@ -4,43 +4,33 @@ using namespace std;
 //옥상 정원 꾸미기
 //2493 탑의 반대문제이라고 생각됨.
 //O(n)
+//스택의 사이즈를 더하는건 지금 있는 건물들에서 벤치마킹 할 수 있는 경우의 수가 되게 함.
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
     int n;
     cin >> n;
 
-    vector<int> vc;
-    //vector에 값을 넣고
+    stack<long long> st;
+    long long ans=0;
     for(int i=0;i<n;i++){
-        int num;
+        long long num;
         cin >> num;
-        vc.push_back(num);
-    } 
-
-    for(int i=0;i<n-1;i++){
-        cout << i+1 << ' ';
-        stack<int> st;
-        for(int j=i;j<n;j++){
-            if(vc[i]<vc[j]){
-                break;
+        while(!st.empty()){
+            if(st.top() <= num){
+                st.pop();
             }
-            //1번 2번 3번 --- 5번까지 관리인이 옥상을 확인함.
-            st.push(vc[j]);
-            cout << st.top() <<' '<< st.size()<<'\n';
         }
-        //cout << '\n';
-    }
-    /* int ans=0;
-    for(auto c: vc){
-        ans+=c;
-    }
-    cout << ans; */
+        st.push(num);
+        ans+=st.size()-1;
+    } 
+    cout << ans;
     return 0;
 }
 
-//10 3 7 4 12 2
-//10 3 7 4 ////12는 10보다 크니까 안됨.
-//3 ////7은 3보다 크니까 안됨.
-//7 4 ////12는 7보다 크니까 안됨.
-//4 ////12는 4보다 크니까 안됨.
-//12 2
-//2는 마지막이므로 확인 X
+//1. 스택의 top에 위치한 빌딩의 높이가 i 번째 빌딩의 높이보다 작거나 같을 경우 i 번째 빌딩의 옥상을 볼 수 없으므로 스택의 top이 i 번째 빌딩의 높이보다 높을 때까지 pop을 해줍니다.
+
+//2. 스택에 i 번째 빌딩의 높이를 push 해줍니다.
+
+//3. i 번째 빌딩을 제외한 나머지 빌딩들이 i 번째 빌딩의 옥상을 볼 수 있으므로 결과에 (스택의 크기 - 1)을 더해줍니다.
