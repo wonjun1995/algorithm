d5f79ffcb2ee7f280fcd76a4489e6e29e8133092
wonjun <wonjun@sysofting.co.kr>
2022-03-08 17:58:02 +0900

    0308_괄호의 값

--- 2022/백준/cpp/스택의활용/2504.cpp
+++ 2022/백준/cpp/스택의활용/2504.cpp
@@ -3,6 +3,33 @@ using namespace std;
 
 //괄호의 값
 int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    string s;
+    cin >> s;
+
+    stack<char> st;
+    int ans=0;
+    int num =1;
+
+    for(int i=0;i<s.size();i++){
+
+        if(s[i] == '('){
+            num*=2;
+            st.push(s[i]);
+
+        }else if(s[i]=='['){
+            num*=3;
+            st.push(s[i]);
+        }else if(s[i] == ')'){
+             if(s[i-1] == '(') ans += num;
+             st.pop();
+             
+        }else{
+            //']'
+        }
+    }
 
     return 0;
 }
