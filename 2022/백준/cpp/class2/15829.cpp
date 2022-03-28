f010cb562e89f0e33b8b8ca03ffacd2d08f3aedd
wonjun1995 <wonjun9506@gmail.com>
2022-03-28 23:29:20 +0900

    0328_class2

---
+++ 2022/백준/cpp/class2/15829.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+const int MOD = 1234567891; 
+const int MULTIPLY = 31;
+
+
+int main(){
+    int L; 
+    cin >> L; 
+    string s;
+     cin >> s;
+    long long sum = 0;
+    long long R = 1; 
+    for (int i = 0; i < s.length(); i++) { 
+        sum = (sum + (s[i] - 'a' + 1) * R) % MOD; 
+        R = (R*MULTIPLY) % MOD; 
+    } 
+    
+    cout << sum << "\n";
+
+
+    return 0;
+}
